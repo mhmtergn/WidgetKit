20fee48e7c6096710ebc0de95716d969f5f7e9a9
Mehmet Ergun <mrmehmetergun@gmail.com>
2022-06-24 01:10:23 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Mehmet Ergün on 2022-06-24.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
